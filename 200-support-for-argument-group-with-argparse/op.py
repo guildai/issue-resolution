1acf9ad0665f1dd2f0062e47891b19bf5e6dce93
Garrett Smith <garrett@placeon.earth>
2021-03-26 08:07:12 -0500

    Use argparse group correctly in issue resolution example

--- 200-support-for-argument-group-with-argparse/op.py
+++ 200-support-for-argument-group-with-argparse/op.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-group = parser.add_argument_group("Data")
-parser.add_argument("--src", default="")
+group = parser.add_argument_group("data")
+group.add_argument("--src", default="")
 
 print(parser.parse_args())
