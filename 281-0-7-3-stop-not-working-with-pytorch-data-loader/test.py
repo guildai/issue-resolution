2e625983134271150a4a502d6589b1838952a13f
Garrett Smith <garrett@placeon.earth>
2021-04-06 16:53:45 -0500

    Tweak to code

--- 281-0-7-3-stop-not-working-with-pytorch-data-loader/test.py
+++ 281-0-7-3-stop-not-working-with-pytorch-data-loader/test.py
@@ -1,9 +1,12 @@
 import multiprocessing as mp
+import os
 
 import mod
 
 count = 5
-sleep = 5.0
+sleep = 99.0
+
+print("Main process %i" % os.getpid())
 
 ps = [mp.Process(target=mod.f, args=[sleep]) for _ in range(count)]
 for p in ps:
